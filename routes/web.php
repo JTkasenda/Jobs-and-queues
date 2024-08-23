d5fa803e8e5a3ebda4aaa0c0c8286c7a3be4f29a
LordSnow <jeantshimbalanga9@gmail.com>
2024-08-23 14:34:26 +0100

    Commiting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/upload', function(){
+    return view('upload-file');
+});
+Route::post('/upload', function (){
+    if(request()->has('mycsv')){
+    $data = array_map('str_getcsv', file(request()->mycsv));
+    $headers = $data[0];
+    unset($data[0]);
+    return $headers;
+
+    }
+    else{
+        return "Please upload file";
+    }
 });
